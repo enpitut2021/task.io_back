6dc6a8042b74ecd30c761cea87d6eb60ccd627fd
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2021-08-02 15:48:02 +0900

    settingsからcorsの削除

--- taskio/settings.py
+++ taskio/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    "corsheaders",
+    #"corsheaders",
 ]
 
 AUTH_USER_MODEL = 'api.User'
@@ -52,7 +52,7 @@ AUTH_USER_MODEL = 'api.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    #"corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,16 +137,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ORIGIN_WHITELIST = {
-    'localhost:3000/',
-    'localhost:3000',
-    '127.0.0.1:3000/',
-    '127.0.0.1:3000',
-    'localhost:8000/',
-    'localhost:8000',
-    '127.0.0.1:8000/',
-    '127.0.0.1:8000',
-}
+#CORS_ORIGIN_WHITELIST = {
+#    'localhost:3000/',
+#    'localhost:3000',
+#    '127.0.0.1:3000/',
+#    '127.0.0.1:3000',
+#    'localhost:8000/',
+#    'localhost:8000',
+#    '127.0.0.1:8000/',
+#    '127.0.0.1:8000',
+#}
 
 try:
     from . import local_settings
