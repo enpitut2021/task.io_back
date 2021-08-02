84cde6a597c436253a40077ec1371cacfe78717c
kanaupi <53474798+kanaupi@users.noreply.github.com>
2021-08-02 16:32:29 +0900

    django-filterで日付検索
    Co-authored-by: tomoki52 <tomoki52@users.noreply.github.com>

--- api/views.py
+++ api/views.py
@@ -9,6 +9,15 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerialzer
 
+class FilterTask(filters.FilterSet):
+    day = filters.DateTimeFilter(field_name='deadline', lookup_expr='gte')
+
+    class Meta:
+        model = Task
+        fields = []
+
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_class = FilterTask
